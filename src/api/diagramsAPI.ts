bb9bfab6df23e3af4903cdc701ee0379d73c0f20
MykytaMaliarenko <mykytamaliarenko@gmail.com>
2019-12-01 02:09:26 +0200

    ref(diagramsAPI)

--- src/api/diagramsAPI.ts
+++ src/api/diagramsAPI.ts
@@ -3,7 +3,7 @@ import {Link} from "../render/link";
 import {Point} from "../render/point";
 
 export const DiagramAPI = {
-    calculateDiagram: (equation: string, dataset: Array<number>): RenderData => {
+    calculateDiagram: async (equation: string, dataset: Array<number>): Promise<RenderData> => {
         return new RenderData(new Array<Point>(), new Array<Link>());
     }
 };
