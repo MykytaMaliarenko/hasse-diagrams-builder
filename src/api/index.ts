0efb3adb6c48f0ac51154e3c3383d5b4cc4a7f70
Nikita <mykytamaliarenko@gmail.com>
2019-12-14 20:33:35 +0200

    feat(api): add post

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,20 @@
 class HttpWrapper {
 
-    constructor(private _backendURL: string, private _defaultHeaders: object) {
+    constructor(private _backendURL: string, private _defaultHeaders: Headers | string[][] | Record<string, string>) {
     }
 
-    get(resource: string, params: object) {
+    get(resource: string) {
 
     }
 
-    post(resource: string, params: object) {
-
+    post(resource: string, body: string) {
+        return fetch(`${this._backendURL}/${resource}`, {
+            method: "POST",
+            mode: "cors",
+            headers: this._defaultHeaders,
+            body
+        })
     }
 }
 
-export const api = new HttpWrapper("/", {});
+export const api = new HttpWrapper("http://127.0.0.1:5000", {});
