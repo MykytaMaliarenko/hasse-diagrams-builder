2730727f31d82c8252ed2fc1658a049d28276870
Nikita <mykytamaliarenko@gmail.com>
2019-12-17 15:09:40 +0200

    ref(diagramsAPI): add headers to request + reworked parsing

--- src/api/diagramsAPI.ts
+++ src/api/diagramsAPI.ts
@@ -5,33 +5,52 @@ import {api} from "./index";
 
 export const DiagramAPI = {
     buildDiagram: async (equation: string, dataset: Array<number>): Promise<RenderData | null> => {
-        let response = await api.post("diagramBuilder", JSON.stringify({
-            equation,
-            dataset
-        }));
+        let response = await api.post("diagramBuilder",
+            JSON.stringify({
+                equation,
+                dataset
+            }),
+            {
+                "Content-Type": "application/json"
+            }
+        );
 
         if (response.ok) {
-            let points = Array<Point>(),
+            let sorted_points = Array<Array<Point>>(),
                 links = Array<Link>();
 
             let responseJson = await response.json();
             // @ts-ignore
-            responseJson.points.forEach(point => {
-                points.push(new Point(
-                    point.id,
-                    point.value,
-                ))
+            responseJson.points.forEach(line => {
+                sorted_points.push([]);
+                // @ts-ignore
+                line.forEach(point => {
+                    sorted_points[sorted_points.length - 1].push(new Point(
+                        point.id,
+                        point.value,
+                    ));
+                });
             });
+
+            let findPoint = (id: number): undefined | Point => {
+                let res: undefined | Point = undefined;
+                sorted_points.forEach(line => {
+                    if (res === undefined) {
+                        res = line.find(point => point.id === id)
+                    }
+                });
+                return res
+            };
 
             // @ts-ignore
             responseJson.links.forEach(link => {
                 links.push(new Link(
-                    points.find(point => point.id === <number> link.x)!!,
-                    points.find(point => point.id === <number> link.y)!!
+                    findPoint(link.x)!!,
+                    findPoint(link.y)!!,
                 ))
             });
 
-            return new RenderData(points, links);
+            return new RenderData(sorted_points, links);
         } else {
             return null;
         }
