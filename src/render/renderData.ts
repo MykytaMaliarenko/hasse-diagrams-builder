3535ebf61766d0ca9dd6bb9aef81c5982d327055
Nikita <mykytamaliarenko@gmail.com>
2019-12-17 15:11:31 +0200

    ref(renderData): points are now kept sorted by rank

--- src/render/renderData.ts
+++ src/render/renderData.ts
@@ -3,9 +3,9 @@ import {Link} from "./link";
 
 export class RenderData {
 
-    constructor(private _points: Array<Point>, private _links: Array<Link>) {}
+    constructor(private _points: Array<Array<Point>>, private _links: Array<Link>) {}
 
-    get points(): Array<Point> {
+    get points(): Array<Array<Point>> {
         return this._points
     }
 
