24d9c46d476a398c3ed882be5a630a6b0c6ddaf9
Nikita <mykytamaliarenko@gmail.com>
2019-12-21 23:51:56 +0200

    ref(app): ref app using new ui

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,20 @@
-import {state, STATE_STAGE1, STATE_STAGE2, STATE_LOADING} from "./state";
-import {datasetValidate, equationValidate} from "./validators";
 import {DiagramAPI} from "./api/diagramsAPI";
 import {CoreRender} from "./render";
 
 import "./styles/index.sass"
-
-const APP_STATE: state = STATE_STAGE1;
+import {UI} from "./ui";
+import {ACTION_GO_TO_RENDER} from "./ui/actions";
 
 window.onload = () => {
-    let equationInput = document.getElementById("equationInput")!!;
-    equationInput.oninput = onEquationInput(equationInput);
+    let ui = new UI(document.body);
+    ui.dispatcher.on(ACTION_GO_TO_RENDER, async (data: {equation: string, set: Array<number>}) => {
+        let renderData = await DiagramAPI.buildDiagram(data.equation, data.set),
+            container = document.getElementById("stage2")!!,
+            mainCanvas = <HTMLCanvasElement> document.getElementById("mainCanvas")!!,
+            subCanvas = <HTMLCanvasElement> document.getElementById("subCanvas")!!;
 
-    let dataSetInput = document.getElementById("setInput")!!;
-    dataSetInput.oninput = onDataSetInput(dataSetInput);
-
-    let buildButton = document.getElementById("buildButton");
-    if (buildButton) {
-        buildButton.onclick = async () => {
-            //add parsing and getting data from user input
-            let equation: string = "",
-                dataset: Array<number> = [];
-
-            if (datasetValidate(dataset) && equationValidate(equation)) {
-                let renderData = await DiagramAPI.buildDiagram(equation, dataset),
-                    container = document.getElementById("stage2")!!,
-                    mainCanvas = <HTMLCanvasElement> document.getElementById("mainCanvas")!!,
-                    subCanvas = <HTMLCanvasElement> document.getElementById("subCanvas")!!;
-
-                let coreRender = new CoreRender(container, mainCanvas, subCanvas);
-                coreRender.renderData = renderData!;
-                coreRender.render();
-            } else {
-                //notify user that input is wrong
-            }
-        };
-    }
-
-};
-
-const onEquationInput = (input: HTMLElement): any => {
-
-};
-
-const onDataSetInput = (input: HTMLElement): any => {
-
+        let coreRender = new CoreRender(container, mainCanvas, subCanvas);
+        coreRender.renderData = renderData!;
+        coreRender.render();
+    });
 };
