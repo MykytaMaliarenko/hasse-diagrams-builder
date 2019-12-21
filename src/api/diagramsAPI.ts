33a7b69af64bb9ff173c49ae12e3bfd35ffd5adc
Nikita <mykytamaliarenko@gmail.com>
2019-12-21 21:17:33 +0200

    remove everything not related to hg-pages

--- src/api/diagramsAPI.ts
+++
@@ -1,9 +0,0 @@
-import {RenderData} from "../render/renderData";
-import {Link} from "../render/link";
-import {Point} from "../render/point";
-
-export const DiagramAPI = {
-    calculateDiagram: async (equation: string, dataset: Array<number>): Promise<RenderData> => {
-        return new RenderData(new Array<Point>(), new Array<Link>());
-    }
-};
--- src/validators/index.ts
+++
@@ -1,7 +0,0 @@
-export const equationValidate = (text: string): boolean => {
-    return false;
-};
-
-export const datasetValidate = (text: Array<number>): boolean => {
-    return false;
-};
