98e47f59352ed25675ad3bbcd57ff8d6bbd7787a
Nikita <mykytamaliarenko@gmail.com>
2019-12-22 15:28:02 +0200

    feat(ui): add all new components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,7 @@
 import {Dispatcher} from "./dispatcher";
 import {EquationInput} from "./stage_1/equationInput";
 import {SetInput} from "./stage_1/setInput";
+import {BuildDiagramButton} from "./stage_1/buildDiagramButton";
 
 export interface UIComponent {
     init(root: HTMLElement, dispatcher: Dispatcher): void
@@ -14,17 +15,20 @@ export class UI {
     public static readonly  components: Array<UIComponent> = [
         new EquationInput(),
         new SetInput(),
+        new BuildDiagramButton()
     ];
 
     public readonly dispatcher: Dispatcher = new Dispatcher();
 
     constructor(private _root: HTMLElement) {
+        document.getElementById("stage2")!!.hidden = true;
         UI.components.forEach((item) => {
-            item.init(this._root, this.dispatcher)
+            item.init(this._root, this.dispatcher);
         });
     }
 
-    private initSecondStage() {
-
+    public showSecondStage() {
+        document.getElementById("stage1")!!.hidden = true;
+        document.getElementById("stage2")!!.hidden = false;
     }
 }
